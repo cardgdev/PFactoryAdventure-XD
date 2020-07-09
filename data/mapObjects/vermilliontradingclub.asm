48eaaf89c4eb992972d5e3c27fd52c652b485fed
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-07-09 18:20:34 -0400

    Vermilion Trade House NPCs implemented

--- data/mapObjects/vermilliontradingclub.asm
+++ data/mapObjects/vermilliontradingclub.asm
@@ -7,7 +7,12 @@ VermillionTradingClubObject:
 
 	db $0 ; signs
 
-	db $0 ; objects
+	db $5 ; objects
+	object SPRITE_OAK_AIDE, $5, $1, STAY, DOWN, $1 ; person
+	object SPRITE_GIRL, $1, $1, STAY, DOWN, $2 ; person
+	object SPRITE_SAILOR, $9, $2, STAY, LEFT, $3 ; person
+	object SPRITE_BLACK_HAIR_BOY_2, $4, $6, STAY, LEFT, $4 ; person
+	object SPRITE_LASS, $1, $3, STAY, DOWN, $5 ; person
 
 	; warp-to
 	EVENT_DISP VERMILLION_TRADING_CLUB_WIDTH, $7, $6
--- scripts/vermilliontradingclub.asm
+++ scripts/vermilliontradingclub.asm
@@ -2,4 +2,41 @@ VermillionTradingClubScript:
 	jp EnableAutoTextBoxDrawing
 
 VermillionTradingClubTextPointers:
+	dw VermillionTradingClubText1
+	dw VermillionTradingClubText2
+	dw VermillionTradingClubText3
+	dw VermillionTradingClubText4
+	dw VermillionTradingClubText5
+
+VermillionTradingClubText1:
+	TX_ASM
+	ld a, $2
+	ld [wWhichTrade], a
+VermillionTradingClubScriptEnd:
+	predef DoInGameTradeDialogue
+	jp TextScriptEnd
+
+VermillionTradingClubText2:
+	TX_ASM
+	ld a, $a
+	ld [wWhichTrade], a
+	jr VermillionTradingClubScriptEnd
+
+VermillionTradingClubText3:
+	TX_ASM
+	ld a, $b
+	ld [wWhichTrade], a
+	jr VermillionTradingClubScriptEnd
+
+VermillionTradingClubText4:
+	TX_ASM
+	ld a, $c
+	ld [wWhichTrade], a
+	jr VermillionTradingClubScriptEnd
+
+VermillionTradingClubText5:
+	TX_ASM
+	ld a, $d
+	ld [wWhichTrade], a
+	jr VermillionTradingClubScriptEnd
 
