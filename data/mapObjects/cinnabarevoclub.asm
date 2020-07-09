a15010bbf886008526c4cb29a97fcbbbf7970af3
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-07-09 19:25:29 -0400

    Evo Stone Club NPCs set up

--- data/mapObjects/cinnabarevoclub.asm
+++ data/mapObjects/cinnabarevoclub.asm
@@ -7,7 +7,14 @@ CinnabarEvoClubObject:
 
 	db $0 ; signs
 
-	db $0 ; objects
+	db $7 ; objects
+	object SPRITE_ROCKER, $1, $1, STAY, DOWN, $1 ; person
+	object SPRITE_LASS, $5, $1, STAY, DOWN, $2 ; person
+	object SPRITE_BLACKBELT, $4, $6, STAY, NONE, $3 ; person
+	object SPRITE_FISHER, $4, $4, STAY, NONE, $4 ; person
+	object SPRITE_BUG_CATCHER, $9, $4, STAY, NONE, $5 ; person
+	object SPRITE_GIRL, $9, $2, STAY, NONE, $6 ; person
+	object SPRITE_OAK_AIDE, $1, $3, STAY, NONE, $7 ; person
 
 	; warp-to
 	EVENT_DISP CINNABAR_EVO_CLUB_WIDTH, $7, $6
--- scripts/cinnabarevoclub.asm
+++ scripts/cinnabarevoclub.asm
@@ -1,5 +1,71 @@
 CinnabarEvoClubScript:
-	jp EnableAutoTextBoxDrawing
+	call EnableAutoTextBoxDrawing
+	ret
 
 CinnabarEvoClubTextPointers:
+	dw ThunderNPCText
+	dw LeafNPCText
+	dw FireNPCText
+	dw WaterNPCText
+	dw HoneyNPCText
+	dw IceNPCText
+	dw MistNPCText
+
+ThunderNPCText:
+	TX_FAR _ThunderNPCText
+	db "@"
+
+LeafNPCText:
+	TX_FAR _LeafNPCText
+	db "@"
+
+FireNPCText:
+	TX_FAR _FireNPCText
+	db "@"
+
+WaterNPCText:
+	TX_FAR _WaterNPCText
+	db "@"
+
+HoneyNPCText:
+	TX_FAR _HoneyNPCText
+	db "@"
+
+IceNPCText:
+	TX_FAR _IceNPCText
+	db "@"
+
+MistNPCText:
+	TX_FAR _MistNPCText
+	db "@"
+
+;-----------------------------------------------------
+
+_ThunderNPCText:
+	text "THUNDER"
+	done
+
+_LeafNPCText:
+	text "LEAF"
+	done
+
+_WaterNPCText:
+	text "WATER"
+	done
+
+_FireNPCText:
+	text "FIRE"
+	done
+
+_HoneyNPCText:
+	text "HONEY"
+	done
+
+_IceNPCText:
+	text "ICE"
+	done
+
+_MistNPCText:
+	text "MIST"
+	done
 
