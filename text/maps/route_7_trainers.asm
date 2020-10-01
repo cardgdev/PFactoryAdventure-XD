19eced44848f9b742fa5f0fc7776736b995eec8b
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-10-01 00:22:44 -0400

    Route 7's new trainers text

--- text/maps/route_7_trainers.asm
+++ text/maps/route_7_trainers.asm
@@ -1,71 +1,108 @@
 _Route7BattleText1::
-	text "DUMMY1"
+	text "I'm a star KDF"
+	line "cadet!"
 	done
 
 _Route7EndBattleText1::
-	text "DUMMY2"
+	text "Out-"
+	line "shined!"
 	prompt
 
 _Route7AfterBattleText1::
-	text "DUMMY3"
+	text "Look at my"
+	line "ribbons!"
+	
+	para "I do more"
+	line "JROTC events"
+	cont "than anyone"
+	cont "at my school!"
 	done
 
 
 
 _Route7BattleText2::
-	text "DUMMY1"
+	text "I'll impress my"
+	line "sargent at"
+	cont "school if I"
+	cont "beat people here!"
 	done
 
 _Route7EndBattleText2::
-	text "DUMMY2"
+	text "Aw"
+	line "man! I'll be"
+	cont "darned!"
 	prompt
 
 _Route7AfterBattleText2::
-	text "DUMMY3"
+	text "We're getting"
+	line "extra credit for"
+	cont "trying our best!"
 	done
 
 
 
 
 _Route7BattleText3::
-	text "DUMMY1"
+	text "I'll be honest"
+	line "I only joined"
+	cont "JROTC to get"
+	cont "out of having"
+	cont "to take gym"
+	cont "class."
 	done
 
 _Route7EndBattleText3::
-	text "DUMMY2"
+	text "Oh"
+	line "jeez..."
 	prompt
 
 _Route7AfterBattleText3::
-	text "DUMMY3"
+	text "There's a lot"
+	line "of nerdy kids"
+	cont "in JROTC who"
+	cont "only join for"
+	cont "that reason."
 	done
 
 
 
 
 _Route7BattleText4::
-	text "DUMMY1"
+	text "At least we"
+	line "didn't have to"
+	cont "wear our stiff"
+	cont "uniforms today."
 	done
 
 _Route7EndBattleText4::
-	text "DUMMY2"
+	text "Ouch!"
 	prompt
 
 _Route7AfterBattleText4::
-	text "DUMMY3"
+	text "Those dress shoes"
+	line "we have to wear"
+	cont "hurt my feet"
+	cont "every time."
 	done
 
 
 
 
 _Route7BattleText5::
-	text "DUMMY1"
+	text "Let me show the"
+	line "others in my"
+	cont "platoon how its"
+	cont "done!"
 	done
 
 _Route7EndBattleText5::
-	text "DUMMY2"
+	text "Taken"
+	line "down! This"
+	cont "won't do!"
 	prompt
 
 _Route7AfterBattleText5::
-	text "DUMMY3"
+	text "Well there goes"
+	line "my rapport."
 	done
 
