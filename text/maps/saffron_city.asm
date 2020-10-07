9206e736335fd6a06630722aa1274d9e9912408e
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2020-10-06 23:57:37 -0400

    Saffron main map text

--- text/maps/saffron_city.asm
+++ text/maps/saffron_city.asm
@@ -1,69 +1,120 @@
 ; protesters
 _SaffronCityText1::
-	text "What do you want?"
-	line "Get lost!"
+	text "The KDF is holding"
+	line "all my co-workers"
+	cont "inside! This is"
+	cont "madness!"
 	done
 
 _SaffronCityText2::
-	text "BOSS said he'll"
-	line "take this town!"
+	text "We're protesting"
+	line "the KDF's abuse"
+	cont "of citizens!"
 	done
 
 _SaffronCityText3::
-	text "Get out of the"
-	line "way!"
+	text "I never thought"
+	line "I'd be defending"
+	cont "a big corporation"
+	cont "like SILPH CO.,"
+	cont "but here we are"
+	cont "unfortunately..."
 	done
 
 _SaffronCityText4::
-	text "SAFFRON belongs"
-	line "to TEAM ROCKET!"
+	text "The KDF's been"
+	line "making trouble"
+	cont "for everyone this"
+	cont "entire crisis!"
+	
+	para "Instead of doing"
+	line "anything to help"
+	cont "people they've"
+	cont "been invading"
+	cont "public spaces and"
+	cont "hiding secrets!"
 	done
 
 _SaffronCityText5::
-	text "Being evil makes"
-	line "me feel so alive!"
+	text "The KDF?"
+	
+	para "I couldn't give"
+	line "a RATATTA's!"
+	cont "I'm just here to"
+	cont "loot stores and"
+	cont "kick trashcans!"
 	done
 
 _SaffronCityText6::
-	text "Ow! Watch where"
-	line "you're walking!"
+	text "We're here to"
+	line "stand up to the"
+	cont "way the KDF is"
+	cont "treating us!"
+	
+	para "They're using"
+	line "the mist crisis"
+	cont "as an excuse to"
+	cont "push us around"
+	cont "and take power!"
 	done
 
 _SaffronCityText7::
-	text "With SILPH under"
-	line "control, we can"
-	cont "exploit #MON"
-	cont "around the world!"
+	text "Hehe I got bussed"
+	line "in here from"
+	cont "FUSCHIA CITY to"
+	cont "make a ruckus and"
+	cont "delegitimatize"
+	cont "real protesters!"
 	done
 
 ; post protest
 _SaffronCityText8::
-	text "You beat TEAM"
-	line "ROCKET all alone?"
-	cont "That's amazing!"
+	text "Even though the"
+	line "KDF left, clean-"
+	cont "ing up the mess"
+	cont "they made will"
+	cont "cost us months"
+	cont "of time and lost"
+	cont "work on SILPH's"
+	cont "latest projects."
 	done
 
 _SaffronCityText9::
-	text "Yeah! TEAM ROCKET"
-	line "is gone!"
-	cont "It's safe to go"
-	cont "out again!"
+	text "So stoked to see"
+	line "my tax dollars"
+	cont "at work funding"
+	cont "state sponsored"
+	cont "terrorism!"
 	done
 
 _SaffronCityText10::
-	text "People should be"
-	line "flocking back to"
-	cont "SAFFRON now."
+	text "We still don't"
+	line "know what the"
+	cont "KDF conducted the"
+	cont "raid on SILPH to"
+	cont "find."
+	
+	para "Maybe it had"
+	line "something to do"
+	cont "with the MASTER"
+	cont "BALL tech they"
+	cont "developed?"
+	
+	para "Would that mean"
+	line "it's capturing"
+	cont "a #MON that"
+	cont "they're after?"
 	done
 
 _SaffronCityText11::
-	text "I flew here on my"
-	line "PIDGEOT when I"
-	cont "read about SILPH."
-
-	para "It's already over?"
-	line "I missed the"
-	cont "media action."
+	text "PIDGEAUTO's cyber-"
+	line "netic eyes see"
+	cont "everything! The"
+	cont "KDF retreated to"
+	cont "the east!"
+	
+	para "Keep an eye out"
+	line "at CINNABAR!"
 	done
 
 _SaffronCityText12::
@@ -72,18 +123,19 @@ _SaffronCityText12::
 	done
 
 _SaffronCityText13::
-	text "I saw ROCKET"
-	line "BOSS escaping"
-	cont "SILPH's building."
+	text "I missed the bus"
+	line "back to FUSCHIA"
+	cont "with the other"
+	cont "paid protesters!"
 	done
 
 ;event extra
 _SaffronCityText14::
-	text "I'm a security"
-	line "guard."
-
-	para "Suspicious kids I"
-	line "don't allow in!"
+	text "Get back!"
+	line "Get back!"
+	
+	para "Leave or face"
+	line "penalty of law!"
 	done
 
 _SaffronCityText15::
@@ -114,25 +166,21 @@ _SaffronCityText18::
 _SaffronCityText20::
 	text "TRAINER TIPS"
 
-	para "FULL HEAL cures"
-	line "all ailments like"
-	cont "sleep and burns."
-
-	para "It costs a bit"
-	line "more, but it's"
-	cont "more convenient."
+	para "If you COUNTER a"
+	line "one-hit-KO move,"
+	cont "the foe will be"
+	cont "instantly KO'd!"
 	done
 
 _SaffronCityText21::
 	text "TRAINER TIPS"
 
-	para "New GREAT BALL"
-	line "offers improved"
-	cont "capture rates."
-
-	para "Try it on those"
-	line "hard-to-catch"
-	cont "#MON."
+	para "You can destroy a"
+	line "SUBSTITUTE with"
+	cont "SELFDESTRUCT or"
+	cont "EXPLOSION without"
+	cont "fainting your"
+	cont "#MON!"
 	done
 
 _SaffronCityText22::
@@ -146,9 +194,12 @@ _SaffronCityText24::
 	done
 
 _SaffronCityText25::
-	text "SILPH's latest"
-	line "product!"
+	text "It's a notice:"
 
-	para "Release to be"
-	line "determined..."
+	para "KDF OPERATING IN"
+	line "SILPH CO. AREA"
+	
+	para "STAND ASIDE IF"
+	line "REQUESTED BY ANY"
+	cont "PERSONNEL."
 	done
